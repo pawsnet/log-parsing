735ba61deaa530b0a98e458c087b5f1179c310f5
Richard Mortier <mort@cantab.net>
2014-09-23 18:06:33 +0200

    vpn-users: coalesce sessions, detect session ends

--- vpn-users.py
+++ vpn-users.py
@@ -20,23 +20,26 @@
 import sys, getopt, re, time
 from enum import Enum
 
+TIMEOUT = 10 * 60
+
 class State(Enum):
     Invalid = 0
     Opening = 1
     Active = 2
 
 class Session:
-    def __init__(self, timestamp, username):
-        self.timestamp = timestamp
+    def __init__(self, open_ts, username):
+        self.open_ts = self.refresh_ts = open_ts
         self.username = username
         self.state = State.Opening
 
     def __str__(self):
         if self.state == State.Active:
-            rv = '%.0f, %s,  %s, %s,%s, "%s"' % (
-                time.mktime(self.timestamp),
+            rv = '%.0f,%.0f,%.0f, %s,  %s, %s,%s, "%s","%s"' % (
+                time.mktime(self.open_ts), time.mktime(self.refresh_ts),
+                time.mktime(self.refresh_ts)-time.mktime(self.open_ts),
                 self.username, self.locip, self.remip, self.rempt,
-                time.asctime(self.timestamp)
+                time.asctime(self.open_ts), time.asctime(self.refresh_ts)
             )
         return rv
 
@@ -52,6 +55,10 @@ def process(filenames):
     ipaddr_re = re.compile(
         ".*MULTI: Learn: (?P<locip>%s) -> (?P<username>\w+)/(?P<remip>%s):(?P<rempt>\d+)$"
         %(ip4_sre, ip4_sre))
+    timeout_re = re.compile(
+        "(?P<username>\w+)/(?P<remip>%s):(?P<rempt>\d+) \[\w+\] Inactivity timeout" % (ip4_sre,))
+    clientexit_re = re.compile(
+        "(?P<username>\w+)/(?P<remip>%s):(?P<rempt>\d+) SIGTERM\[soft,remote-exit\] received, client-instance exiting" % (ip4_sre,))
 
     # ...client-instance exiting$
     # ...client-instance restarting$
@@ -61,6 +68,7 @@ def process(filenames):
 
     ## initialise local state
     session = None
+    sessions = {}
 
     for fn in filenames:
         with open(fn) as f:
@@ -73,25 +81,78 @@ def process(filenames):
                 timestamp, line = line[:15], line[40:].strip()
                 timestamp = time.strptime("2014 "+timestamp, ts_format)
 
-                ## is this a session opening?
-                m = login_re.match(line)
-                if m:
-                    username = m.group("username")
-                    session = Session(timestamp, username)
-
-                ## bail if we've no session in flight
-                if not session: continue
-
                 ## is this a session completing opening?
                 m = ipaddr_re.match(line)
                 if m:
-                    ## barf if overlapping session
-                    if session.username != m.group("username"): BARF
-                    (session.locip, session.remip, session.rempt) = (
+                    username = m.group("username")
+                    if username not in sessions: continue
+                    session = sessions[username]
+
+                    (locip, remip, rempt) = (
                         m.group("locip"), m.group("remip"), m.group("rempt"))
 
-                    session.state = State.Active
-                    print(session)
+                    if session.state != State.Active:
+                        session.locip = locip
+                        session.remip = remip
+                        session.rempt = rempt
+                        session.state = State.Active
+
+                    else:
+                        session.refresh_ts = timestamp
+                        if Verbose:
+                            if (locip, remip, rempt) != (
+                                    session.locip, session.remip, session.rempt
+                            ):
+                                ## change of remote port; new device?
+                                print(session, ", new-device")
+                                session.locip = locip
+                                session.remip = remip
+                                session.rempt = rempt
+                                session.open_ts = timestamp
+                                session.refresh_ts = timestamp
+
+                    sessions[username] = session
+                    continue
+
+                ## is this a session timing out?
+                m = timeout_re.match(line)
+                if m:
+                    username = m.group("username")
+                    if username not in sessions: continue
+                    session = sessions[username]
+
+                    ## best estimate we have for when session went idle
+                    session.refresh_ts = timestamp ## time.gmtime(time.mktime(timestamp) - TIMEOUT)
+
+                    print(session, ", timeout")
+                    del sessions[username]
+                    continue
+
+                ## is this a session just closing?
+                m = clientexit_re.match(line)
+                if m:
+                    username = m.group("username")
+                    if username not in sessions: continue
+                    session = sessions[username]
+
+                    ## best estimate we have for when session went idle
+                    session.refresh_ts = timestamp ## time.gmtime(time.mktime(timestamp) - TIMEOUT)
+                    print(session, ", client-exit")
+                    del sessions[username]
+                    continue
+
+                ## is this a new session?
+                m = login_re.match(line)
+                if m:
+                    username = m.group("username")
+                    if username in sessions: session = sessions[username]
+                    else:
+                        session = Session(timestamp, username)
+                        sessions[username] = session
+
+    for session in sessions.values():
+        session.refresh_ts = timestamp
+        print(session, ", eof")
 
 if __name__ == '__main__':
 
@@ -105,7 +166,7 @@ if __name__ == '__main__':
     except getopt.GetoptError as err: die_with_usage(err, 2)
 
     output = sys.stdout
-    Verbose = True
+    Verbose = False
     try:
         for o, a in opts:
             if o in ("-h", "--help"): die_with_usage()
